4767877c20d25bda2ecd2f18e3f5f28e719cfd69
Mahendra Kavathiya <mahendra@virtueinfo.com>
2013-07-22 00:34:06 +0530

    Initial repositery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photostore::Application.config.secret_key_base = '73dd4f11f574ec9d73c8e033abf5bc4b5cd91389cd43717d9ec8363ab8d7ddd3776c4ac72293eeed0cd8c22ff8eb4f107e8f794680116cb01aee9dbe001a4ed6'
